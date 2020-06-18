add864038381b5febbe47c56170f326d90f95f78
joan <joan.herisson@univ-evry.fr>
2020-06-18 20:27:29 +0200

    import sys

--- src/brs_utils/print.py
+++ src/brs_utils/print.py
@@ -4,16 +4,17 @@ Created on June 16 2020
 @author: Joan Hérisson
 """
 
+from sys import stdout as sys_stdout
 
 def print_OK(time=-1):
-    sys.stdout.write("\033[0;32m") # Green
+    sys_stdout.write("\033[0;32m") # Green
     print(" OK", end = '', flush=True)
-    sys.stdout.write("\033[0;0m") # Reset
+    sys_stdout.write("\033[0;0m") # Reset
     if time!=-1: print(" (%.2fs)" % time, end = '', flush=True)
     print()
 
 def print_FAILED():
-    sys.stdout.write("\033[1;31m") # Red
+    sys_stdout.write("\033[1;31m") # Red
     print(" Failed")
-    sys.stdout.write("\033[0;0m") # Reset
+    sys_stdout.write("\033[0;0m") # Reset
     print()
