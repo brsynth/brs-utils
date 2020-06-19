dfeaa620082384f3af02d5fac3703430b4d8b4bb
joan <joan.herisson@univ-evry.fr>
2020-06-19 13:50:57 +0200

    ver

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="brs_utils",
-    version="0.0.7",
+    version="0.0.8",
     author="Joan Hérisson, Melchior du Lac",
     author_email="joan.herisson@univ-evry.fr",
     description="Utilities",
