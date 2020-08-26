d7de5a6f00a2629d170055f52abf72cbe6c738b2
joan <joan.herisson@univ-evry.fr>
2020-08-26 16:09:18 +0200

    add read_dict

---
+++ tests/test_ReadDict.py
@@ -0,0 +1,25 @@
+"""
+Created on June 17 2020
+
+@author: Joan Hérisson
+"""
+
+from unittest import TestCase
+import json
+
+from brs_utils import read_dict
+
+
+class Test_ReadDict(TestCase):
+
+    d = {'a': 1, 'b': 2}
+
+    def _create_file_from_dict(self, filename, dict):
+        with open(filename, 'w') as f:
+            f.write(json.dumps(dict))
+
+    def test_readdict_ok(self):
+        filename = 'dict'
+        _create_file_from_dict(filename, d)
+        dict = read_dict(filename)
+        self.assertDictEqual(dict, d)
