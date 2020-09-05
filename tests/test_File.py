944e44dfd214cf5205c81ec8b256339023c2813e
joan <joan.herisson@univ-evry.fr>
2020-09-05 16:48:33 +0200

    mv test read_dict into test_file.py

--- tests/test_File.py
+++ tests/test_File.py
@@ -6,8 +6,9 @@ Created on Jul 14 2020
 
 from unittest import TestCase
 
-from brs_utils import file_length
+from brs_utils import file_length, read_dict
 from pathlib import Path
+from json import dumps as json_dumps
 
 class Test_File(TestCase):
 
@@ -19,3 +20,15 @@ class Test_File(TestCase):
 
     def test_100l_file(self):
         self.assertEqual(file_length('data/100l_file.txt'), 100)
+
+    d = {'a': 1, 'b': 2}
+
+    def _create_file_from_dict(self, filename, dict):
+        with open(filename, 'w') as f:
+            f.write(json_dumps(dict))
+
+    def test_readdict_ok(self):
+        filename = 'dict'
+        self._create_file_from_dict(filename, self.d)
+        dict = read_dict(filename)
+        self.assertDictEqual(dict, self.d)
