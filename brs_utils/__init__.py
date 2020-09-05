62aa795c26dd8d12d5d66a962b0a6a959e9ff5b7
joan <joan.herisson@univ-evry.fr>
2020-09-05 16:42:04 +0200

    declare read_dict

--- brs_utils/__init__.py
+++ brs_utils/__init__.py
@@ -6,10 +6,10 @@ Created on June 16 2020
 
 from .rpSBML import rpSBML
 from .misc   import total_size, check_nb_args
-from .file   import download, extract_gz, download_and_extract_gz, file_length
+from .file   import download, extract_gz, download_and_extract_gz, file_length, read_dict
 from .print  import print_OK, print_FAILED
 
 __all__ = ('rpSBML',
            'total_size', 'check_nb_args',
-           'file_length', 'download', 'extract_gz', 'download_and_extract_gz',
+           'file_length', 'download', 'extract_gz', 'download_and_extract_gz', 'read_dict'
            'print_OK', 'print_FAILED')
