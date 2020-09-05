fbc0922084ed07ea3b3eaa88fb8df88869aa79e4
joan <joan.herisson@univ-evry.fr>
2020-09-05 17:02:31 +0200

    fix url+hashes

--- tests/test_File.py
+++ tests/test_File.py
@@ -6,9 +6,11 @@ Created on Jul 14 2020
 
 from unittest import TestCase
 
-from brs_utils import file_length, read_dict
-from pathlib import Path
-from json import dumps as json_dumps
+from brs_utils import file_length, read_dict, download, extract_gz, download_and_extract_gz
+from tempfile  import NamedTemporaryFile, TemporaryDirectory
+from pathlib   import Path
+from hashlib   import sha256
+from json      import dumps as json_dumps
 
 class Test_File(TestCase):
 
@@ -21,7 +23,7 @@ class Test_File(TestCase):
     def test_100l_file(self):
         self.assertEqual(file_length('data/100l_file.txt'), 100)
 
-    d = {'a': 1, 'b': 2}
+    _d = {'a': 1, 'b': 2}
 
     def _create_file_from_dict(self, filename, dict):
         with open(filename, 'w') as f:
@@ -29,6 +31,25 @@ class Test_File(TestCase):
 
     def test_readdict_ok(self):
         filename = 'dict'
-        self._create_file_from_dict(filename, self.d)
+        self._create_file_from_dict(filename, self._d)
         dict = read_dict(filename)
-        self.assertDictEqual(dict, self.d)
+        self.assertDictEqual(dict, self._d)
+
+    _url = 'https://gitlab.com/breakthewall/rpcache-data/-/raw/master/metanetx/comp_xref.tsv.gz'
+
+    def test_download(self):
+        with NamedTemporaryFile() as tempf:
+            download(self._url,
+                     tempf.name)
+            self.assertEqual(
+                sha256(Path(tempf.name).read_bytes()).hexdigest(),
+                '7b9a3931d850821b7d5b443767f8c5282fd879ab71eb455c8809c009f8ca7dd7'
+                            )
+
+    def test_extract_gz(self):
+        with TemporaryDirectory() as tempd:
+            extract_gz('data/data.gz', tempd)
+            self.assertEqual(
+                sha256(Path(tempd+'/test_Download.py').read_bytes()).hexdigest(),
+                '504676268634b8c340a11e202b4d9c7cc08f9daea749f4c9cf5db9294772bc39'
+                            )
