49c7dc526a4b30556cec8e48a603debd42a95883
joan <joan.herisson@univ-evry.fr>
2020-09-16 15:33:36 +0200

    fix 'insert_and_or_replace_in_sorted_list' function

--- brs_utils/list.py
+++ brs_utils/list.py
@@ -12,17 +12,18 @@ from bisect import insort as bisect_insort
 #  @param list sorted list of items to insert item
 #  @return updated list
 def insert_and_or_replace_in_sorted_list(item, list):
-
     # If present, remove the same item with worse score from the list
     try:
         i = list.index(item)
+        # new item's score is better than the one already in the list, then replace it
         if item > list[i]:
             list.pop(i)
+            # Insert at the good place current item in the list
+            # If the item's score is lower that the last item of the list, then it will be added at the end and cut later when only topX will be kept
+            bisect_insort(list, item)
+    # Same item does not exist in the list, then insert it
     except ValueError:
-        pass
-
-    # Insert at the good place current item in the list
-    # If the item's score is lower that the last item of the list, then it will be added at the end and cut later when only topX will be kept
-    bisect_insort(list, item)
-
+        # Insert at the good place current item in the list
+        # If the item's score is lower that the last item of the list, then it will be added at the end and cut later when only topX will be kept
+        bisect_insort(list, item)
     return list
