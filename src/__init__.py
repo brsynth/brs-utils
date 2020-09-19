37e9af28881d5ab29056133712d28963732b74bd
joan <joan.herisson@univ-evry.fr>
2020-09-19 10:42:54 +0200

    absolute imports

--- src/__init__.py
+++ src/__init__.py
@@ -4,10 +4,10 @@ Created on June 16 2020
 @author: Joan Hérisson
 """
 
-from .misc   import total_size, check_nb_args
-from .file   import download, extract_gz, extract_tar_gz, extract_gz_to_string, download_and_extract_gz, download_and_extract_tar_gz, file_length, read_dict
-from .print  import print_OK, print_FAILED
-from .list   import insert_and_or_replace_in_sorted_list
+from brs_utils.misc   import total_size, check_nb_args
+from brs_utils.file   import download, extract_gz, extract_tar_gz, extract_gz_to_string, download_and_extract_gz, download_and_extract_tar_gz, file_length, read_dict
+from brs_utils.print  import print_OK, print_FAILED
+from brs_utils.list   import insert_and_or_replace_in_sorted_list
 
 __all__ = ('total_size', 'check_nb_args',
            'file_length', 'download', 'extract_gz', 'extract_tar_gz', 'extract_gz_to_string', 'download_and_extract_gz', 'download_and_extract_tar_gz', 'file_length', 'read_dict',
