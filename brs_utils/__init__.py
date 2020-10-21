fa99dc367e6ccda7460065c4c9b41adc9ef5dba7
joan <joan.herisson@univ-evry.fr>
2020-10-21 15:21:19 +0200

    ci(trigger): just to trigger Test suite

--- brs_utils/__init__.py
+++ brs_utils/__init__.py
@@ -9,6 +9,7 @@ from brs_utils.file   import download, extract_gz, extract_tar_gz, extract_gz_to
 from brs_utils.print  import print_OK, print_FAILED
 from brs_utils.list   import insert_and_or_replace_in_sorted_list
 
+
 __all__ = ('total_size', 'check_nb_args',
            'file_length', 'download', 'extract_gz', 'extract_tar_gz', 'extract_gz_to_string', 'download_and_extract_gz', 'download_and_extract_tar_gz', 'file_length', 'read_dict',
            'print_OK', 'print_FAILED',
