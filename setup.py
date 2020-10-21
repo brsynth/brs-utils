e2e529e1b61e4b397846aa7d0bca28c4a1fe653f
joan <joan.herisson@univ-evry.fr>
2020-10-21 15:16:37 +0200

    refactor(conda): switch to pure conda packaging

--- setup.py
+++ setup.py
@@ -1,46 +1,48 @@
 from setuptools import setup
-from pathlib import Path
 
 _readme = 'README.md'
 
-_extras_path = 'extras'
-
-with open(_extras_path+'/.env', 'r') as f:
-    line = f.readline()
-    if line.startswith('PACKAGE='):
-        _package = line.splitlines()[0].split('=')[1].lower()
-
 with open(_readme, 'r') as f:
     long_description = f.read()
-
-required = []
-with open(_extras_path+'/requirements.txt', 'r') as f:
-    required = [line[:-1] for line in f]
 
 _release = 'RELEASE'
 
 with open(_release, 'r') as f:
     _version = f.readline().split()[0]
 
+_extras_path = 'extras'
+with open(_extras_path+'/.env', 'r') as f:
+    for line in f:
+        if line.startswith('PACKAGE='):
+            _package = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('URL='):
+            _url = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('AUTHORS='):
+            _authors = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('DESCR='):
+            _descr = line.splitlines()[0].split('=')[1].lower()
+        if line.startswith('CORR_AUTHOR='):
+            _corr_author = line.splitlines()[0].split('=')[1].lower()
+
 setup(
-    name=_package,
-    version=_version,
-    author='Melchior du Lac, Joan Hérisson',
-    author_email='joan.herisson@univ-evry.fr',
-    description='BRSynth Utilities',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/brsynth/brs-utils',
-    packages=[_package],
-    package_dir={_package: 'src'},
-    include_package_data=True,
-    install_requires=required,
-    test_suite='discover_tests',
-    license='MIT',
+    name                          = _package,
+    version                       = _version,
+    author                        = _authors,
+    author_email                  = _corr_author,
+    description                   = _descr,
+    long_description              = long_description,
+    long_description_content_type = 'text/markdown',
+    url                           = _url,
+    packages                      = [_package],
+    package_dir                   = {_package: _package},
+    include_package_data          = True,
+    # install_requires              = required,
+    test_suite                    = 'pytest',
+    license                       = 'MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.5',
+    python_requires               = '>=3.5',
 )
