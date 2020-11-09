912ffd5c7f292b819f366ed3e3494904ec5f2d75
joan <joan.herisson@univ-evry.fr>
2020-11-09 22:50:29 +0100

    ci(test): remove 'test' parsing

--- ci/test/parse_recipe.py
+++ ci/test/parse_recipe.py
@@ -1,7 +1,8 @@
-from yaml   import safe_load as yaml_safe_load
-from yaml   import YAMLError
-from os     import path      as os_path
-from shutil import copyfile
+from yaml     import safe_load as yaml_safe_load
+from yaml     import YAMLError
+from os       import path      as os_path
+from shutil   import copyfile
+from tempfile import NamedTemporaryFile
 
 current_folder = os_path.dirname(os_path.realpath(__file__))
 
@@ -10,8 +11,10 @@ channels_file = current_folder+'/../../recipe/conda_channels.txt'
 recipe_file   = current_folder+'/../../recipe/meta.yaml'
 bld_cfg_file  = current_folder+'/../../recipe/conda_build_config.yaml'
 # output files
-env_file      = current_folder+'/test-environment.yml'
-test_file     = current_folder+'/test.sh'
+# with NamedTemporaryFile(delete=False) as d:
+# outdir        = mktemp()
+# env_file      = outdir+'/test-environment.yml'
+# test_file     = current_folder+'/test.sh'
 
 def parse_meta(filename):
 
@@ -33,32 +36,37 @@ def parse_meta(filename):
         except TypeError: pass
         try: requirements += yaml_safe_load(recipe)['test']['requires']
         except TypeError: pass
-        tests['commands']     = yaml_safe_load(recipe)['test']['commands']
-        tests['source_files'] = yaml_safe_load(recipe)['test']['source_files']
+        # tests['commands']     = yaml_safe_load(recipe)['test']['commands']
+        # tests['source_files'] = yaml_safe_load(recipe)['test']['source_files']
     except YAMLError as exc:
         print(exc)
 
-    return requirements, tests
+    return requirements
 
-def parse_build_config(filename):
-    with open(filename, 'r') as f:
-        f_configs = f.read()
-        try: configs = yaml_safe_load(f_configs)
-        except TypeError: pass
-    return configs
+# def parse_build_config(filename):
+#     with open(filename, 'r') as f:
+#         f_configs = f.read()
+#         try: configs = yaml_safe_load(f_configs)
+#         except TypeError: pass
+#     return configs
 
 
-def write_dependencies(filename, requirements):
+def write_dependencies(requirements):
 
-    channels = open(channels_file, 'r').read().split()
+    with NamedTemporaryFile(delete=False, suffix='.yml') as f:
 
-    with open(filename, 'w') as f:
-        f.write('channels:\n')
+        channels = open(channels_file, 'r').read().split()
+
+        f.write(str.encode('channels:\n'))
         for c in channels:
-            f.write('  - '+c+'\n')
-        f.write('dependencies:\n')
+            f.write(str.encode('  - '+c+'\n'))
+        f.write(str.encode('dependencies:\n'))
         for req in requirements:
-            f.write('  - '+req+'\n')
+            f.write(str.encode('  - '+req+'\n'))
+
+        f.seek(0)
+
+        return f.name
 
 def write_tests(filename, tests):
     with open(filename, 'w') as f:
@@ -68,13 +76,15 @@ def write_tests(filename, tests):
             f.write(source_files)
 
 
-requirements, tests = parse_meta(recipe_file)
-write_dependencies(env_file, requirements)
-write_tests(test_file, tests)
+requirements = parse_meta(recipe_file)
+env_file = write_dependencies(requirements)
+# write_tests(test_file, tests)
 
-bld_cfgs = parse_build_config(bld_cfg_file)
-for pkg in bld_cfgs:
-    for ver in bld_cfgs[pkg]:
-        env_file_cfg = env_file+'.'+pkg+str(ver)
-        dep = pkg+' '+str(ver)
-        print(dep)
+print(env_file)
+
+# bld_cfgs = parse_build_config(bld_cfg_file)
+# for pkg in bld_cfgs:
+#     for ver in bld_cfgs[pkg]:
+#         env_file_cfg = env_file+'.'+pkg+str(ver)
+#         dep = pkg+' '+str(ver)
+#         print(dep)
