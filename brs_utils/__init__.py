1df6efdf4b7d610ed4c27e635e84a2200fc269ff
Joan Hérisson <joan.herisson@univ-evry.fr>
2026-04-08 11:58:59 +0200

    trigger

--- brs_utils/__init__.py
+++ brs_utils/__init__.py
@@ -53,14 +53,3 @@ from brs_utils.parser import build_args_parser as build_args_parser
 from brs_utils.string import comp_succ as comp_succ
 from brs_utils.Cache import Cache as Cache
 from brs_utils._version import __version__ as __version__
-
-# __all__ = (
-#     'total_size', 'check_nb_args',
-#     'file_length', 'download', 'read_dict',
-#     'compress_tar_gz', 'compress_gz',
-#     'extract_gz', 'extract_tar_gz', 'extract_gz_to_string',
-#     'download_and_extract_gz', 'download_and_extract_tar_gz',
-#     'print_OK', 'print_FAILED',
-#     'insert_and_or_replace_in_sorted_list',
-#     'create_logger'
-# )
